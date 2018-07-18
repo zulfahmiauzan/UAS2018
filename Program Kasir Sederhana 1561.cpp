29849c0d0b5030c1b014127131974b0a2319de8b
zulfahmiauzan <41378578+zulfahmiauzan@users.noreply.github.com>
2018-07-19 00:16:22 +0700

    Add files via upload

---
+++ Program Kasir Sederhana 1561.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int x,produk1=10000,produk2=20000,produk3=15000,jumlah1,jumlah2,jumlah3,total,bayar,tunai,sisa,diskon;
+string barang1,barang2,barang3;
+
+	   cout<<"============================================"<<endl;
+	   cout<<"  Program Kasir Sederhana Zul Fahmi Auzan"<<endl;
+	   cout<<"============================================"<<endl;
+	   cout<<endl;
+
+			cout<<"Masukkan Nama Produk 1		: "; cin>>barang1;
+			cout<<"Jumlah Produk			: ";
+			cin>>x;
+			jumlah1=x*produk1;
+			cout<<"Total Harga			: Rp. "<<jumlah1<<endl;cout<<endl; 
+
+			cout<<"Masukkan Nama Produk 2		: "; cin>>barang2;
+			cout<<"Jumlah Produk			: ";
+			cin>>x;
+			jumlah2=x*produk2;
+			cout<<"Total Harga			: Rp. "<<jumlah2<<endl;cout<<endl; 
+			
+			cout<<"Masukkan Nama Produk 3		: "; cin>>barang3;
+			cout<<"Jumlah Produk			: ";
+			cin>>x;
+			jumlah3=x*produk3;
+			cout<<"Total Harga			: Rp. "<<jumlah3<<endl;cout<<endl; 
+
+			bayar=jumlah1+jumlah2+jumlah3;
+			cout<<endl; 
+			cout<<"============================================"<<endl;
+			cout<<"Sub Total			: Rp. "<<bayar<<endl;
+
+if(bayar>50000)
+{
+	diskon=0.10*bayar;
+	cout<<"Diskon 10%, diskon sebesar	: Rp. "<<diskon<<endl;
+	total=bayar-diskon;
+	cout<<"Total				: Rp. "<<total<<endl;
+}
+else
+if(bayar<50000)
+{
+	diskon=0;
+	cout<<"Diskon 0%, diskon sebesar	: Rp. "<<diskon<<endl;
+	total=bayar;
+	cout<<"Total				: Rp. "<<total<<endl;
+}
+	cout<<"============================================"<<endl;
+	cout<<"Uang tunai			: Rp. ";
+	cin>>tunai;
+	sisa=tunai-total;
+	cout<<"Kembalian			: Rp. "<<sisa<<endl;
+	cout<<endl; 
+}
